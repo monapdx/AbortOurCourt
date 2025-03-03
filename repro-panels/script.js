2322412f2d135b4701eae72f1581b396fdf2d464
monapdx <thegirlnextfloor@gmail.com>
2025-03-02 17:54:55 -0800

    Add files via upload

--- repro-panels/script.js
+++ repro-panels/script.js
@@ -20,3 +20,16 @@ function addCloseButtons() {
 }
 
 addCloseButtons();
+
+	$('#promo').wScratchPad({
+    // eraser size
+    size        : 40,    
+    // image underneath that's being revealed
+    bg          : "plan-c-pills.png",
+    // image above that is being scratched off
+    fg: 'red.gif',
+    // The cursor (coin) image
+    cursor    : 'url("coin1.png") 5 5, default',
+      // give real-time updates
+    realtime    : true
+ });
